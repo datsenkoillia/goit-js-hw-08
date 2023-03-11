8ac5dd01bdc228c0c55c0e7c9693b127d5e48179
Illia Datsenko <datsenko.i@gmail.com>
2023-03-11 21:43:29 +0200

    2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import trottle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function (data) {
+  console.log(data);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  ),
+    1000;
+};
+
+player.on('timeupdate', trottle(onPlay, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
