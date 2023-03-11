7b1eefc34db13a72fb51f9c199723de66b1fe9f5
Illia Datsenko <datsenko.i@gmail.com>
2023-03-11 20:27:45 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 // console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 
